100bf39b12d4304e108b72a7e2ff87948fd145b9
algoritmika-ekb <pashamikov@hotmail.com>
2026-04-19 16:34:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
-racker1 = Player('racker.png', 30, 200,4,50,150)
-racker2 = Player('racker.png',30,200,4,50,150)
+racket1 = Player('racker.png', 30, 200,4,50,150)
+racket2 = Player('racker.png',30,200,4,50,150)
 
 ball = GameSprite('tenis_ball.png', 200, 200, 4 , 50, 50)
 
@@ -49,3 +49,23 @@ font.init()
 font = font.Font(None,35)
 lose1 = font.render('1 ИГРОК неВИНЕР', True, (180,0,0))
 lose2 = font.render('2 ИГРОК неВИНЕР', True, (180,0,0))
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+    display.update()
+    clock.tick(FPS)
